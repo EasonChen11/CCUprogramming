8e4f996a04590954046aa6ee65a95512565b330c
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-21 20:10:34 +0800

    finish

--- week6-2.c
+++ week6-2.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int compare(const void *a,const void *b){
+
+void swap(char s[1000000], int j);
+
+int compare(const void *a, const void *b){
     return *(char*)a>*(char*)b;
 }
 int main(){
-    char s[1000000]={""};
+    char s[1000000];
     printf("please enter a string=");
     fgets(s,sizeof (s),stdin);
-    for (int i = 0; i < strlen(s); ++i) {
+    for (int i = 0; i < strlen(s)-1; ++i) {
         if(s[i]>96)
             s[i]-=32;
     }
     qsort(s, strlen(s)-1,sizeof (char),compare);
+    /*for(int i=1;i< strlen(s)-1;i++){
+        int j=i;
+        while (s[j]<s[j-1] && j< strlen(s)-1){
+            swap(s,j);
+            j--;
+        }*/
+    }
     printf("after sorting=");
     puts(s);
+}
+
+void swap(char s[1000000], int j) {
+    int x=s[j];
+    s[j]=s[j-1];
+    s[j-1]=x;
 }
